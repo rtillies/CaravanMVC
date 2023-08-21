e9dc677a7d65488688d48585895b40f25a805d96
Richard T <richard@turing.edu>
2023-08-21 15:24:07 -0400

    Seed database

--- CaravanMVC/DataAccess/CaravanMvcContext.cs
+++ CaravanMVC/DataAccess/CaravanMvcContext.cs
@@ -13,5 +13,21 @@ namespace CaravanMVC.DataAccess
 
 		}
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Wagon>().HasData(
+				new Wagon { Id = 1, Name = "Lexi Silver", NumWheels = 4, IsCovered = true },
+				new Wagon { Id = 2, Name = "Clifford", NumWheels = 4, IsCovered = false }
+			);
+
+			modelBuilder.Entity<Passenger>().HasData(
+				new Passenger { Id = 1, Name = "Professor", Age = 28, Destination = "Virginia", WagonId = 1 },
+				new Passenger { Id = 1, Name = "Bella", Age = 22, Destination = "North Carolina", WagonId = 2 },
+				new Passenger { Id = 1, Name = "Seneca", Age = 28, Destination = "Virginia", WagonId = 1 },
+				new Passenger { Id = 1, Name = "Carolyn", Age = 35, Destination = "Virginia", WagonId = 2 }
+			);
+		}
+
+
 	}
 }
