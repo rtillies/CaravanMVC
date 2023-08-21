8520a1c97791bc36521462b1a1edc4de81f8a23b
Richard T <richard@turing.edu>
2023-08-21 17:36:11 -0400

    Statistics routes

---
+++ CaravanMVC/Controllers/StatisticsController.cs
@@ -0,0 +1,30 @@
+﻿using CaravanMVC.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaravanMVC.Controllers
+{
+	public class StatisticsController : Controller
+	{
+		private readonly CaravanMvcContext _context;
+
+		public StatisticsController(CaravanMvcContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var wagons = _context.Wagons.Include(w => w.Passengers).ToList();
+
+			var destinations = new List<string>();
+			foreach(var wagon in wagons)
+			{
+				destinations.AddRange(wagon.getDestinationList());
+			}
+			ViewData["DestinationList"] = destinations.Distinct().ToList();
+
+			return View(wagons);
+		}
+	}
+}
