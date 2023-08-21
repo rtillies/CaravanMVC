98ff24235afc475cf598fffe70b5d5d70ac4bea4
Richard T <richard@turing.edu>
2023-08-21 15:11:36 -0400

    Wagon model created

---
+++ CaravanMVC/Models/Wagon.cs
@@ -0,0 +1,10 @@
+﻿namespace CaravanMVC.Models
+{
+	public class Wagon
+	{
+        public string Name { get; set; }
+        public int NumWheels { get; set; }
+        public bool IsCovered { get; set; }
+        public List<Passenger> Passengers { get; set; } = new List<Passenger>();
+    }
+}
