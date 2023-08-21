9ab85bb8ccb0c9ab8b0aaed09d3aa43ce75e9424
Richard T <richard@turing.edu>
2023-08-21 16:55:54 -0400

    Passenger #New route

---
+++ CaravanMVC/Controllers/PassengersController.cs
@@ -0,0 +1,49 @@
+﻿using CaravanMVC.DataAccess;
+using CaravanMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaravanMVC.Controllers
+{
+	public class PassengersController : Controller
+	{
+		private readonly CaravanMvcContext _context;
+
+		public PassengersController(CaravanMvcContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Route("wagons/{wagonId:int}/passengers/new")]
+		public IActionResult New(int wagonId)
+		{
+			var wagon = _context.Wagons
+				.Where(w => w.Id == wagonId)
+				.Include(w => w.Passengers)
+				.First();
+
+			return View(wagon);
+		}
+
+		[HttpPost]
+		[Route("wagons/{wagonId:int}")]
+		public IActionResult Create(int wagonId, Passenger passenger)
+		{
+			var wagon = _context.Wagons
+				.Where(w => w.Id == wagonId)
+				.Include(w => w.Passengers)
+				.First();
+
+			wagon.Passengers.Add(passenger);
+			_context.SaveChanges();
+
+			//return RedirectToAction("index", new { wagonId = wagon.Id });
+			return Redirect($"/wagons/{wagon.Id}");
+		}
+	}
+}
