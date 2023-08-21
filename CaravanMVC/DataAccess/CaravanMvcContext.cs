60782fe2a6790015b4258f36fbc61c9af63aae95
Richard T <richard@turing.edu>
2023-08-21 15:13:36 -0400

    Update Context

--- CaravanMVC/DataAccess/CaravanMvcContext.cs
+++ CaravanMVC/DataAccess/CaravanMvcContext.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CaravanMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CaravanMVC.DataAccess
 {
     public class CaravanMvcContext : DbContext
     {
-    }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Wagon> Wagons { get; set; }
+
+		public CaravanMvcContext(DbContextOptions<CaravanMvcContext> options) : base(options)
+		{
+
+		}
+
+	}
 }
