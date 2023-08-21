6d51ff8c5cf9560f5e06aeaffcfc85433f155ba2
Richard T <richard@turing.edu>
2023-08-21 15:23:46 -0400

    Add Id and WagonId fields

--- CaravanMVC/Models/Passenger.cs
+++ CaravanMVC/Models/Passenger.cs
@@ -2,9 +2,11 @@
 {
 	public class Passenger
 	{
+		public int Id { get; set; }
 		public string Name { get; set; }
         public int Age { get; set; }
         public string Destination { get; set; }
         public Wagon Wagon { get; set; }
+		public int WagonId { get; set; }
     }
 }
