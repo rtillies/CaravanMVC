ced5b3351548992aac675ab6d8593a2223fc9d7c
Richard T <richard@turing.edu>
2023-08-21 16:03:45 -0400

    Wagon #Index route

---
+++ CaravanMVC/Controllers/WagonsController.cs
@@ -0,0 +1,21 @@
+﻿using CaravanMVC.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaravanMVC.Controllers
+{
+    public class WagonsController : Controller
+    {
+        private readonly CaravanMvcContext _context;
+
+        public WagonsController(CaravanMvcContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var wagons = _context.Wagons.ToList();
+            return View(wagons);
+        }
+    }
+}
